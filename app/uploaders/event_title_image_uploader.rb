9966905b3d0b5a9f36e0922d504d1303caed10a3
Sergey Kovalevsky <sk@httplab.ru>
2016-04-25 01:51:37 +0300

    WIP Index и show страницы мероприятия

--- app/uploaders/event_title_image_uploader.rb
+++ app/uploaders/event_title_image_uploader.rb
@@ -15,26 +15,6 @@ class EventTitleImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
   version :square_500 do
     process resize_to_fill: [500, 500]
   end
