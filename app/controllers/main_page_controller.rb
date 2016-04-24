2778c94f3dcb5e18ed2f19e088df4dd6300b584a
Sergey Kovalevsky <sk@httplab.ru>
2016-04-24 17:53:52 +0300

    Работа над лейаутом и главной

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,6 +1,18 @@
 class MainPageController < ApplicationController
   layout 'it61-template'
 
+  before_filter :fetch_counters
+
   def show
+  end
+
+  private
+
+  def fetch_counters
+    @counters ||= {
+      users: User.count,
+      events: Event.published.count,
+      companies: Company.count
+    }
   end
 end
